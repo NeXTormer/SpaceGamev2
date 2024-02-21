fca4793f4c8958cee54a26828ff5496a544ea97a
Felix Holz <felix.holz@me.com>
2024-02-22 00:18:34 +0100

    fix xcode project repo

---
+++ xcode-project/spacegame/spacegame/spacegameApp.swift
@@ -0,0 +1,17 @@
+//
+//  spacegameApp.swift
+//  spacegame
+//
+//  Created by Felix Holz on 19.02.24.
+//
+
+import SwiftUI
+
+@main
+struct spacegameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
